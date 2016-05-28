b75d928a38aa4e2816050a61ed3a9ee84ede750d
Julien Tellier <jtellier@octo.com>
2016-05-28 16:33:56 +0200

    imported base configuration for production and dev settings

--- learn/services/choice.py
+++ learn/services/choice.py
@@ -4,9 +4,10 @@ from itertools import chain
 from django.db.models import Q
 from django.utils import timezone
 
+from learn.infrastructure.configuration import get_configuration
 from learn.models import Translation, RythmNotation
 
-MAX_WORDS = 10
+MAX_WORDS = 'LEARN_MAX_WORDS'
 
 
 def random_choice(dictionary_pk):
@@ -28,10 +29,11 @@ def get_next_word(dictionary_pk, tomorrow, user):
     words = Translation.objects \
         .filter(dictionary_id=dictionary_pk, rythmnotation__user=user, rythmnotation__next_repetition__lt=tomorrow) \
         .order_by('?')
-    if len(words) < MAX_WORDS:
+    max_words = int(get_configuration(MAX_WORDS))
+    if len(words) < max_words:
         translations = Translation.objects.filter(
                 Q(dictionary_id=dictionary_pk),
-                ~Q(rythmnotation__user=user))[:(MAX_WORDS - len(words))]
+                ~Q(rythmnotation__user=user))[:(max_words - len(words))]
         for translation in translations:
             RythmNotation.objects.create(user=user, translation=translation, successes=0,
                                          next_repetition=timezone.now())
@@ -43,9 +45,10 @@ def get_next_word(dictionary_pk, tomorrow, user):
 
 
 def prepare_next_batch(dictionary_pk, user):
+    max_words = int(get_configuration(MAX_WORDS))
     new_batch_of_translations = Translation.objects.filter(
             Q(dictionary_id=dictionary_pk),
-            ~Q(rythmnotation__user=user))[:MAX_WORDS]
+            ~Q(rythmnotation__user=user))[:max_words]
     for translation in new_batch_of_translations:
         RythmNotation.objects.create(user=user, translation=translation, successes=0,
                                      next_repetition=timezone.now())
--- learn/services/repetition.py
+++ learn/services/repetition.py
@@ -3,10 +3,13 @@ import math
 
 import django
 
-SUCCESS_MULTIPLIER = 2
-BASE_SECONDS = 2
+from learn.infrastructure.configuration import get_configuration
+
+LEARN_RYTHM_MULTIPLIER = 'LEARN_RYTHM_MULTIPLIER'
+LEARN_BASE_RYTHM = 'LEARN_BASE_RYTHM'
 
 
 def compute_next_repetition(successes):
-    next_repetition_delta = BASE_SECONDS * math.pow(SUCCESS_MULTIPLIER, successes)
+    next_repetition_delta = float(get_configuration(LEARN_BASE_RYTHM)) *\
+                            math.pow(float(get_configuration(LEARN_RYTHM_MULTIPLIER)), successes)
     return django.utils.timezone.now() + datetime.timedelta(seconds=next_repetition_delta)
